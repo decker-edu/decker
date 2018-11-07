ec8f8e5fbe66de445f43bf7148995b64aa74325c
sam17kd <samantha.monty@uni-wuerzburg.de>
2018-11-07 16:34:53 +0100

    Remove stylesheet link in deck.html, require font css files in decker.js

--- src-support/decker.js
+++ src-support/decker.js
@@ -3,6 +3,8 @@ Reveal = require('reveal.js/js/reveal');
 require('reveal.js/css/reveal.scss');
 require('reveal.js/css/theme/white.css');
 require('./decker.scss');
+require('./fonts/roboto.css');
+require('./fonts/source-code-pro.css');
 
 if(window.location.search.match( /print-pdf/gi )){
   require('reveal.js/css/print/pdf.css');
