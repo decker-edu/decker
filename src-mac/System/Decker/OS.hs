c9ecae28de86610fb295e89ee78fa09183a695ba
Jan-Philipp Stauffert <jan-philipp.stauffert@uni-wuerzburg.de>
2019-01-10 14:43:04 +0100

    Fix URL paths for windows

--- src-mac/System/Decker/OS.hs
+++ src-mac/System/Decker/OS.hs
@@ -1,9 +1,13 @@
 {-- Author: Jan-Philipp Stauffert <jan-philipp.stauffert@uni-wuerzburg.de.de> --}
 module System.Decker.OS
   ( defaultProvisioning
+  , urlPath
   ) where
 
 import Common
 
 defaultProvisioning :: Provisioning
 defaultProvisioning = SymLink
+
+urlPath :: FilePath -> FilePath
+urlPath path = path
--- src-win/System/Decker/OS.hs
+++ src-win/System/Decker/OS.hs
@@ -1,9 +1,13 @@
 {-- Author: Jan-Philipp Stauffert <jan-philipp.stauffert@uni-wuerzburg.de.de> --}
 module System.Decker.OS
   ( defaultProvisioning
+  , urlPath
   ) where
 
 import Common
 
 defaultProvisioning :: Provisioning
 defaultProvisioning = Copy
+
+urlPath :: FilePath -> FilePath
+urlPath path = intercalate "/" (splitDirectories path)
