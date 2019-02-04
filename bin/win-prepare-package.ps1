b5e3e8372be8f2f802ee10ebf4bbd72485cc5172
Jan-Philipp Stauffert <jan-philipp.stauffert@uni-wuerzburg.de>
2019-02-04 13:32:25 +0100

    Copy the preparation of the packaging into the build script

--- bin/win-prepare-package.ps1
+++
@@ -1,9 +0,0 @@
-$binpath=(Join-Path ($(stack path | Select-String -Pattern "local-install-root") -split " ")[1] "bin\decker.exe")
-Copy-Item $binpath .
-
-$version = Get-Content .\package.yaml | Select-String -Pattern "version: "
-$version = $version -replace "\s+", " "
-$version = ($version -split " ")[1]
-Write-Output $version > version.txt
-
-Compress-Archive -Force -Path .\resource -CompressionLevel Fastest -DestinationPath resource
