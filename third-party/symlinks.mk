a875c86f1688f0a6b29165a7dba005663243ccc3
Mario Botsch <botsch@techfak.uni-bielefeld.de>
2019-10-05 21:43:55 +0200

    fix resources, MacOS shading bug

--- third-party/symlinks.mk
+++ third-party/symlinks.mk
@@ -12,7 +12,7 @@ else
 dup = ln -sF
 endif
 
-all: jquery chart.js mathjax reveal.js bootstrap piklor.js mb-reveal-plugins fontawesome reveal.js-menu thebelab
+all: jquery chart.js mathjax reveal.js bootstrap piklor.js whiteboard math fontawesome reveal.js-menu thebelab mb-reveal-plugins
 
 thebelab: thebelab/lib/index.js
 	@mkdir -p $(support)/thebelab
@@ -54,6 +54,9 @@ piklor.js:
 
 mb-reveal-plugins:
 	@$(dup) $(third)/mb-reveal-plugins $(support)/
+
+whiteboard:
+	@$(dup) $(third)/mb-reveal-plugins/whiteboard $(support)/whiteboard
 
 math:
 	@$(dup) $(third)/mb-reveal-plugins/math $(support)/math
