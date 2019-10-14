d33096559b9d026a60d11ad2a93b1210a12fc975
Armin Bernstetter <armin.bernstetter@uni-wuerzburg.de>
2019-10-14 12:59:29 +0200

    fix codeblock labels not showing in handouts (+ CI fix)

--- resource/support/js/handout.js
+++ resource/support/js/handout.js
@@ -4,8 +4,20 @@
 // require("bootstrap/dist/css/bootstrap.css");
 // require("./handout.scss");
 
+addSourceCodeLabels();
+
 document.addEventListener("load", () => {
+  console.log("test");
   $("table").addClass(
     "table table-striped table-bordered table-hover table-condensed table-responsive"
   );
 });
+
+function addSourceCodeLabels() {
+  $("div.sourceCode[label]").each(function () {
+    $("<div/>")
+      .addClass("language-label")
+      .text($(this).attr("label"))
+      .prependTo($(this).children('pre'));
+  });
+}
