765885b3369ee0cdd20fc803eb77ec2aafbe0d40
Armin Bernstetter <armin.bernstetter@uni-wuerzburg.de>
2020-03-27 12:49:23 +0100

    parsing to the internal datatype more or less done

--- src/Text/Decker/Filter/NewQuiz.hs
+++ src/Text/Decker/Filter/NewQuiz.hs
@@ -102,25 +102,17 @@ parseQuizzes pandoc = return $ walk parseQuizboxes pandoc
 
 parseQuizboxes :: Block -> Block
 parseQuizboxes d@(Div (id_, tgs@("box":cls), kvs) blocks)
-  | any (`elem` cls) ["qmi", "quiz-mi", "quiz-match-items"]
-    -- parseMatching blocks 
-   =
-    Div
-      (id_, "box" : cls, kvs)
-      [Para [Str (parseAndSet (set tags tgs defaultMatch) blocks)]]
+  | any (`elem` cls) ["qmi", "quiz-mi", "quiz-match-items"] =
+    quizDiv defaultMatch
   | any (`elem` cls) ["qmc", "quiz-mc", "quiz-multiple-choice"] =
-    Div
-      (id_, "box" : cls, kvs)
-      [Para [Str (parseAndSet (set tags tgs defaultMC) blocks)]]
+    quizDiv defaultMC
   | any (`elem` cls) ["qic", "quiz-ic", "quiz-insert-choices"] =
-    Div
-      (id_, "box" : cls, kvs)
-      [Para [Str (parseAndSet (set tags tgs defaultIC) blocks)]]
-  | any (`elem` cls) ["qft", "quiz-ft", "quiz-free-text"] =
-    Div
-      (id_, "box" : cls, kvs)
-      [Para [Str (parseAndSet (set tags tgs defaultFree) blocks)]]
+    quizDiv defaultIC
+  | any (`elem` cls) ["qft", "quiz-ft", "quiz-free-text"] = quizDiv defaultFree
   | otherwise = d
+  where
+    quizDiv q =
+      Div (id_, tgs, kvs) [Para [Str (parseAndSet (set tags tgs q) blocks)]]
 parseQuizboxes bl = bl
 
 defaultMatch = MatchItems [Str "Empty"] [] "" "" [] []
@@ -134,25 +126,16 @@ defaultFree = FreeText [Str "Empty"] [] "" "" [] []
 setTags :: Quiz -> [T.Text] -> Quiz
 setTags q ts = set tags ts q
 
+-- | Temporary
 parseAndSet :: Quiz -> [Block] -> T.Text
-parseAndSet q bs = T.pack $ show $ (foldr parseAndSetQuiz q bs)
+parseAndSet q bs = T.pack $ show (foldr parseAndSetQuiz q bs)
 
-parseMatching :: Quiz -> [Block] -> T.Text
-parseMatching q bs = T.pack $ show $ (foldr (\b -> parseMatching_ b) q bs)
-  -- (foldr (\b -> setQuizHeader b . setQuizMeta b . setMatchList b) q bs)
-
-parseMC :: Quiz -> [Block] -> T.Text
-parseMC q bs = T.pack $ show $ (foldr (\b -> parseMC_ b) q bs)
-
-parseIC :: Quiz -> [Block] -> T.Text
-parseIC q bs = T.pack $ show $ (foldr (\b -> parseIC_ b) q bs)
-
-parseFree :: Quiz -> [Block] -> T.Text
-parseFree q bs = T.pack $ show $ (foldr (\b -> parseFree_ b) q bs)
-
+-- | This monolithic function parses a Pandoc Block and uses lenses to set the field in the given quiz item
 parseAndSetQuiz :: Block -> Quiz -> Quiz
+-- Set the title
 parseAndSetQuiz (Header 2 (id_, cls, kvs) text) q = set title text q
--- Set meta
+-- Set the meta information
+-- Probably should be outsourced to (a) separate function(s)
 parseAndSetQuiz (CodeBlock (id_, cls, kvs) code) q =
   if "yaml" `elem` cls
     then (setCategory . setLectureID) q
@@ -190,33 +173,18 @@ parseAndSetQuiz (DefinitionList items) quiz@(MatchItems ti ta cat id_ q p) =
     parseDL (i, (is, bs)) = Pair i is bs
 -- parse and set choices for FreeText
 parseAndSetQuiz (BulletList blocks) quiz@(FreeText ti ta cat id_ q ch) =
-  set choices (map (parseTL quiz) blocks) quiz
-  where
-    parseTL _ (Plain (Str "☒":Space:is):bs) = Choice True is bs
-    parseTL _ (Plain (Str "☐":Space:is):bs) = Choice False is bs
-    parseTL (FreeText title ts cat qId questions chs) (Plain is:bs) =
-      Choice True is bs
-    parseTL _ is = Choice False [Str "NoTasklistItem"] [Plain []]
+  set choices (map (parseQuizTLItem quiz) blocks) quiz
 -- parse and Set choices for InsertChoices
 parseAndSetQuiz (BulletList blocks) quiz@(InsertChoices ti ta cat id_ q) =
-  set questions (([], map parseTL blocks) : q) quiz
-  where
-    parseTL (Plain (Str "☒":Space:is):bs) = Choice True is bs
-    parseTL (Plain (Str "☐":Space:is):bs) = Choice False is bs
-    parseTL is = Choice False [Str "NoTasklistItem"] [Plain []]
+  set questions (([], map (parseQuizTLItem quiz) blocks) : q) quiz
 -- Parse and set choices for MultipleChoice
 parseAndSetQuiz (BulletList blocks) quiz@(MultipleChoice ti ta cat id_ q ch) =
-  set choices (map parseTL blocks) quiz
-  where
-    parseTL (Plain (Str "☒":Space:is):bs) = Choice True is bs
-    parseTL (Plain (Str "☐":Space:is):bs) = Choice False is bs
-    parseTL is = Choice False [Str "NoTasklistItem"] [Plain []]
--- Set question for matching
+  set choices (map (parseQuizTLItem quiz) blocks) quiz
+-- Parse and set question for matching
 parseAndSetQuiz b quiz@(MatchItems ti ta cat id_ q p) =
   set question (b : q) quiz
 -- Parse and set questions for FreeText
-parseAndSetQuiz b quiz@(FreeText ti ta cat id_ q ch) =
-  set question (q ++ [b]) quiz
+parseAndSetQuiz b quiz@(FreeText ti ta cat id_ q ch) = set question (b : q) quiz
 -- Set question for InsertChoices
 parseAndSetQuiz b quiz@(InsertChoices ti ta cat id_ q) =
   set questions (([b], []) : q) quiz
@@ -224,208 +192,19 @@ parseAndSetQuiz b quiz@(InsertChoices ti ta cat id_ q) =
 parseAndSetQuiz b quiz@(MultipleChoice ti ta cat id_ q ch) =
   set question (b : q) quiz
 -- Default
-parseAndSetQuiz b q = q
+parseAndSetQuiz _ q = q
+
+parseQuizTLItem :: Quiz -> [Block] -> Choice
+parseQuizTLItem _ (Plain (Str "☒":Space:is):bs) = Choice True is bs
+parseQuizTLItem _ (Plain (Str "☐":Space:is):bs) = Choice False is bs
+parseQuizTLItem (FreeText title ts cat qId questions chs) (Plain is:bs) =
+  Choice True is bs
+parseQuizTLItem _ is = Choice False [Str "NoTasklistItem"] [Plain []]
 
 -- | Parse and set the Quiz title from the h2 header
 setQuizHeader :: Block -> Quiz -> Quiz
 setQuizHeader (Header 2 (id_, cls, kvs) text) q = set title text q
 setQuizHeader _ q = q
-
-setMatchList :: Block -> Quiz -> Quiz
-setMatchList (DefinitionList items) quiz@(MatchItems ti ta cat id_ q p) =
-  set pairs (map parseDL (zip [0 .. length items] items)) quiz
-  where
-    parseDL :: (Int, ([Inline], [[Block]])) -> Match
-    parseDL (i, (Str "!":_, bs)) = Distractor bs
-    parseDL (i, (is, bs)) = Pair i is bs
-
-parseMatchList b q = q
-
-parseMatching_ :: Block -> Quiz -> Quiz
--- Set quiz title
-parseMatching_ (Header 2 (id_, cls, kvs) text) q = set title text q
--- Set quiz pairs/Match Items
-parseMatching_ (DefinitionList items) quiz@(MatchItems ti ta cat id_ q p) =
-  set pairs (map parseDL (zip [0 .. length items] items)) quiz
-  where
-    parseDL :: (Int, ([Inline], [[Block]])) -> Match
-    parseDL (i, (Str "!":_, bs)) = Distractor bs
-    parseDL (i, (is, bs)) = Pair i is bs
--- Set meta
-parseMatching_ (CodeBlock (id_, cls, kvs) code) q =
-  if "yaml" `elem` cls
-    then (setCategory . setLectureID) q
-    else q
-  where
-    meta = decodeYaml code
-    quizMetaOptions :: [String]
-    quizMetaOptions = ["category", "score", "lectureID", "topic"]
-    setCategory :: Quiz -> Quiz
-    setCategory q =
-      case getMetaString "category" meta of
-        Just s -> set category (T.pack s) q
-        Nothing -> q
-    -- setScore :: Quiz -> Meta -> Quiz
-    -- setScore q =
-    --   case getMetaString "score" meta of
-    --     Just s -> set score (T.pack s) q
-    --     Nothing -> q
-    setLectureID :: Quiz -> Quiz
-    setLectureID q =
-      case getMetaString "lectureId" meta of
-        Just s -> set qId (T.pack s) q
-        Nothing -> q
-    decodeYaml :: T.Text -> Meta
-    decodeYaml text =
-      case decodeEither' (encodeUtf8 text) of
-        Right a -> toPandocMeta a
-        Left exception -> Meta M.empty
--- Set question for matching
-parseMatching_ b quiz@(MatchItems ti ta cat id_ q p) = set question (b : q) quiz
-
-parseFree_ :: Block -> Quiz -> Quiz
--- Set quiz title
-parseFree_ (Header 2 (id_, cls, kvs) text) q = set title text q
--- Set meta
-parseFree_ (CodeBlock (id_, cls, kvs) code) q =
-  if "yaml" `elem` cls
-    then (setCategory . setLectureID) q
-    else q
-  where
-    meta = decodeYaml code
-    quizMetaOptions :: [String]
-    quizMetaOptions = ["category", "score", "lectureID", "topic"]
-    setCategory :: Quiz -> Quiz
-    setCategory q =
-      case getMetaString "category" meta of
-        Just s -> set category (T.pack s) q
-        Nothing -> q
-    -- setScore :: Quiz -> Meta -> Quiz
-    -- setScore q =
-    --   case getMetaString "score" meta of
-    --     Just s -> set score (T.pack s) q
-    --     Nothing -> q
-    setLectureID :: Quiz -> Quiz
-    setLectureID q =
-      case getMetaString "lectureId" meta of
-        Just s -> set qId (T.pack s) q
-        Nothing -> q
-    decodeYaml :: T.Text -> Meta
-    decodeYaml text =
-      case decodeEither' (encodeUtf8 text) of
-        Right a -> toPandocMeta a
-        Left exception -> Meta M.empty
--- choices
-parseFree_ (BulletList blocks) quiz@(FreeText ti ta cat id_ q ch) =
-  set choices (map (parseTL quiz) blocks) quiz
-  where
-    parseTL _ (Plain (Str "☒":Space:is):bs) = Choice True is bs
-    parseTL _ (Plain (Str "☐":Space:is):bs) = Choice False is bs
-    parseTL (FreeText title ts cat qId questions chs) (Plain is:bs) =
-      Choice True is bs
-    parseTL _ is = Choice False [Str "NoTasklistItem"] [Plain []]
--- Set question for Freetext
-parseFree_ b quiz@(FreeText ti ta cat id_ q ch) = set question (q ++ [b]) quiz
-
-parseIC_ :: Block -> Quiz -> Quiz
--- Set quiz title
-parseIC_ (Header 2 (id_, cls, kvs) text) q = set title text q
--- Set meta
-parseIC_ (CodeBlock (id_, cls, kvs) code) q =
-  if "yaml" `elem` cls
-    then (setCategory . setLectureID) q
-    else q
-  where
-    meta = decodeYaml code
-    quizMetaOptions :: [String]
-    quizMetaOptions = ["category", "score", "lectureID", "topic"]
-    setCategory :: Quiz -> Quiz
-    setCategory q =
-      case getMetaString "category" meta of
-        Just s -> set category (T.pack s) q
-        Nothing -> q
-    -- setScore :: Quiz -> Meta -> Quiz
-    -- setScore q =
-    --   case getMetaString "score" meta of
-    --     Just s -> set score (T.pack s) q
-    --     Nothing -> q
-    setLectureID :: Quiz -> Quiz
-    setLectureID q =
-      case getMetaString "lectureId" meta of
-        Just s -> set qId (T.pack s) q
-        Nothing -> q
-    decodeYaml :: T.Text -> Meta
-    decodeYaml text =
-      case decodeEither' (encodeUtf8 text) of
-        Right a -> toPandocMeta a
-        Left exception -> Meta M.empty
--- choices
-parseIC_ (BulletList blocks) quiz@(InsertChoices ti ta cat id_ q) =
-  set questions (([], map parseTL blocks) : q) quiz
-  where
-    parseTL (Plain (Str "☒":Space:is):bs) = Choice True is bs
-    parseTL (Plain (Str "☐":Space:is):bs) = Choice False is bs
-    -- parseTL (FreeText title ts cat qId questions chs) (Plain is:bs) =
-    --   Choice True is bs
-    parseTL is = Choice False [Str "NoTasklistItem"] [Plain []]
--- Set question for Freetext
-parseIC_ b quiz@(InsertChoices ti ta cat id_ q) =
-  set questions (([b], []) : q) quiz
-
-parseMC_ :: Block -> Quiz -> Quiz
--- Set quiz title
-parseMC_ (Header 2 (id_, cls, kvs) text) q = set title text q
--- Set quiz pairs/Match Items
-parseMC_ (BulletList blocks) quiz@(MultipleChoice ti ta cat id_ q ch) =
-  set choices (map parseTL blocks) quiz
-  where
-    parseTL (Plain (Str "☒":Space:is):bs) = Choice True is bs
-    parseTL (Plain (Str "☐":Space:is):bs) = Choice False is bs
-    -- parseTL (FreeText title ts cat qId questions chs) (Plain is:bs) =
-      -- Choice True is bs
-    parseTL is = Choice False [Str "NoTasklistItem"] [Plain []]
--- Set meta
-parseMC_ (CodeBlock (id_, cls, kvs) code) q =
-  if "yaml" `elem` cls
-    then (setCategory . setLectureID) q
-    else q
-  where
-    meta = decodeYaml code
-    quizMetaOptions :: [String]
-    quizMetaOptions = ["category", "score", "lectureID", "topic"]
-    setCategory :: Quiz -> Quiz
-    setCategory q =
-      case getMetaString "category" meta of
-        Just s -> set category (T.pack s) q
-        Nothing -> q
-    -- setScore :: Quiz -> Meta -> Quiz
-    -- setScore q =
-    --   case getMetaString "score" meta of
-    --     Just s -> set score (T.pack s) q
-    --     Nothing -> q
-    setLectureID :: Quiz -> Quiz
-    setLectureID q =
-      case getMetaString "lectureId" meta of
-        Just s -> set qId (T.pack s) q
-        Nothing -> q
-    decodeYaml :: T.Text -> Meta
-    decodeYaml text =
-      case decodeEither' (encodeUtf8 text) of
-        Right a -> toPandocMeta a
-        Left exception -> Meta M.empty
--- Set question for matching
-parseMC_ b quiz@(MultipleChoice ti ta cat id_ q ch) = set question (b : q) quiz
-parseMC_ b q = q
-
-parseQuizTaskList :: Quiz -> Block -> Either Block [Choice]
-parseQuizTaskList q (BulletList blocks) = Right (map (parseTL q) blocks)
-  where
-    parseTL _ (Plain (Str "☒":Space:is):bs) = Choice True is bs
-    parseTL _ (Plain (Str "☐":Space:is):bs) = Choice False is bs
-    parseTL (FreeText title ts cat qId questions chs) (Plain is:bs) =
-      Choice True is bs
-    parseTL _ is = Choice False [Str "NoTasklistItem"] [Plain []]
-parseQuizTaskList q b = Left b
 
 -- | Meta options for quizzes: score, category, lectureID, topic
 setQuizMeta :: Block -> Quiz -> Quiz
