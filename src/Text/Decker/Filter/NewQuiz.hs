1d13e989c741bbb9eec0bdd7e8dc7a902d65ef64
Armin Bernstetter <armin.bernstetter@uni-wuerzburg.de>
2020-03-30 13:28:09 +0200

    safety commit 2

--- src/Text/Decker/Filter/NewQuiz.hs
+++ src/Text/Decker/Filter/NewQuiz.hs
@@ -6,11 +6,16 @@ import Control.Lens hiding (Choice)
 import qualified Data.Text as T
 import Data.Text.Encoding as E
 import Data.Yaml
+import Text.Blaze.Html as Blaze
+import Text.Blaze.Html.Renderer.String as S
+import qualified Text.Blaze.Html5 as H
+import qualified Text.Blaze.Html5.Attributes as A
 import Text.Pandoc.Definition
 import Text.Pandoc.Shared
 import Text.Pandoc.Walk
 
 import qualified Data.Map.Strict as M
+import Text.Decker.Filter.Local
 import Text.Decker.Internal.Common
 import Text.Decker.Internal.Meta
 
@@ -103,15 +108,16 @@ handleQuizzes pandoc = return $ walk parseQuizboxes pandoc
     parseQuizboxes :: Block -> Block
     parseQuizboxes d@(Div (id_, tgs@("box":cls), kvs) blocks)
       | any (`elem` cls) ["qmi", "quiz-mi", "quiz-match-items"] =
-        renderQuizzes (parseQuizzes (set tags tgs defaultMatch) blocks)
+        renderQuizzes (parseAndSetQuiz (set tags tgs defaultMatch) blocks)
       | any (`elem` cls) ["qmc", "quiz-mc", "quiz-multiple-choice"] =
-        renderQuizzes (parseQuizzes (set tags tgs defaultMC) blocks)
+        renderQuizzes (parseAndSetQuiz (set tags tgs defaultMC) blocks)
       | any (`elem` cls) ["qic", "quiz-ic", "quiz-insert-choices"] =
-        renderQuizzes (parseQuizzes (set tags tgs defaultIC) blocks)
+        renderQuizzes (parseAndSetQuiz (set tags tgs defaultIC) blocks)
       | any (`elem` cls) ["qft", "quiz-ft", "quiz-free-text"] =
-        renderQuizzes (parseQuizzes (set tags tgs defaultFree) blocks)
+        renderQuizzes (parseAndSetQuiz (set tags tgs defaultFree) blocks)
       | otherwise = d
     parseQuizboxes bl = bl
+    -- Give the tag-/classlist of the surrounding div box to the quiz
     setTags :: Quiz -> [T.Text] -> Quiz
     setTags q ts = set tags ts q
     -- The default "new" quizzes
@@ -131,20 +137,20 @@ renderQuizzes quiz@(InsertChoices ti tgs cat id_ q) =
 renderQuizzes quiz@(MultipleChoice ti tgs cat id_ q ch) =
   Div ("", tgs, []) [Para [Str (T.pack $ show quiz)]]
 
--- 
-parseQuizzes :: Quiz -> [Block] -> Quiz
-parseQuizzes q bs = combineICQuestions (foldr parseAndSetQuiz q bs)
+-- Takes a Quiz and a list of blocks, parses the blocks and modifies the given quiz
+parseAndSetQuiz :: Quiz -> [Block] -> Quiz
+parseAndSetQuiz q bs = combineICQuestions (foldr parseAndSetQuizFields q bs)
 
 -- | This function combines the (Question, Choices) tuples of InsertChoice questions
 -- These tuples are of type ([Block], [Choice])
--- This is because in `parseAndSetQuiz` they are created alternatingly as (bs, []) and ([],chs)
+-- This is because in `parseAndSetQuizFields` they are created alternatingly as (bs, []) and ([],chs)
 -- This combine function makes it so that the questions field in InsertChoice is 
 -- an alternating list of "Question text -> quiz element -> question text" etc
 combineICQuestions :: Quiz -> Quiz
 combineICQuestions quiz@(InsertChoices ti tgs cat id_ q) =
   set questions (combineQTuples q) quiz
     -- These tuples can only be ([],a) or (a,[]) per default
-    -- They're created like this in parseAndSetQuiz
+    -- They're created like this in parseAndSetQuizFields
   where
     combineQTuples :: [([Block], [Choice])] -> [([Block], [Choice])]
     combineQTuples bc@[(a, b)] = bc
@@ -157,12 +163,12 @@ combineICQuestions quiz@(InsertChoices ti tgs cat id_ q) =
 combineICQuestions q = q
 
 -- | This monolithic function parses a Pandoc Block and uses lenses to set the field in the given quiz item
-parseAndSetQuiz :: Block -> Quiz -> Quiz
+parseAndSetQuizFields :: Block -> Quiz -> Quiz
 -- Set the title
-parseAndSetQuiz (Header 2 (id_, cls, kvs) text) q = set title text q
+parseAndSetQuizFields (Header 2 (id_, cls, kvs) text) q = set title text q
 -- Set the meta information
 -- Probably should be outsourced to (a) separate function(s)
-parseAndSetQuiz (CodeBlock (id_, cls, kvs) code) q =
+parseAndSetQuizFields (CodeBlock (id_, cls, kvs) code) q =
   if "yaml" `elem` cls
     then (setCategory . setLectureID) q
     else q
@@ -191,34 +197,35 @@ parseAndSetQuiz (CodeBlock (id_, cls, kvs) code) q =
         Right a -> toPandocMeta a
         Left exception -> Meta M.empty
 -- Set quiz pairs/Match Items
-parseAndSetQuiz (DefinitionList items) quiz@(MatchItems ti ta cat id_ q p) =
+parseAndSetQuizFields (DefinitionList items) quiz@(MatchItems ti ta cat id_ q p) =
   set pairs (map parseDL (zip [0 .. length items] items)) quiz
   where
     parseDL :: (Int, ([Inline], [[Block]])) -> Match
     parseDL (i, (Str "!":_, bs)) = Distractor bs
     parseDL (i, (is, bs)) = Pair i is bs
 -- parse and set choices for FreeText
-parseAndSetQuiz (BulletList blocks) quiz@(FreeText ti ta cat id_ q ch) =
+parseAndSetQuizFields (BulletList blocks) quiz@(FreeText ti ta cat id_ q ch) =
   set choices (map (parseQuizTLItem quiz) blocks) quiz
 -- parse and Set choices for InsertChoices
-parseAndSetQuiz (BulletList blocks) quiz@(InsertChoices ti ta cat id_ q) =
+parseAndSetQuizFields (BulletList blocks) quiz@(InsertChoices ti ta cat id_ q) =
   set questions (([], map (parseQuizTLItem quiz) blocks) : q) quiz
 -- Parse and set choices for MultipleChoice
-parseAndSetQuiz (BulletList blocks) quiz@(MultipleChoice ti ta cat id_ q ch) =
+parseAndSetQuizFields (BulletList blocks) quiz@(MultipleChoice ti ta cat id_ q ch) =
   set choices (map (parseQuizTLItem quiz) blocks) quiz
 -- Parse and set question for matching
-parseAndSetQuiz b quiz@(MatchItems ti ta cat id_ q p) =
+parseAndSetQuizFields b quiz@(MatchItems ti ta cat id_ q p) =
   set question (b : q) quiz
 -- Parse and set questions for FreeText
-parseAndSetQuiz b quiz@(FreeText ti ta cat id_ q ch) = set question (b : q) quiz
+parseAndSetQuizFields b quiz@(FreeText ti ta cat id_ q ch) =
+  set question (b : q) quiz
 -- Set question for InsertChoices
-parseAndSetQuiz b quiz@(InsertChoices ti ta cat id_ q) =
+parseAndSetQuizFields b quiz@(InsertChoices ti ta cat id_ q) =
   set questions (([b], []) : q) quiz
 -- Set question for Multiple Choice
-parseAndSetQuiz b quiz@(MultipleChoice ti ta cat id_ q ch) =
+parseAndSetQuizFields b quiz@(MultipleChoice ti ta cat id_ q ch) =
   set question (b : q) quiz
 -- Default
-parseAndSetQuiz _ q = q
+parseAndSetQuizFields _ q = q
 
 parseQuizTLItem :: Quiz -> [Block] -> Choice
 parseQuizTLItem _ (Plain (Str "☒":Space:is):bs) = Choice True is bs
@@ -231,35 +238,22 @@ parseQuizTLItem _ is = Choice False [Str "NoTasklistItem"] [Plain []]
 setQuizHeader :: Block -> Quiz -> Quiz
 setQuizHeader (Header 2 (id_, cls, kvs) text) q = set title text q
 setQuizHeader _ q = q
-
--- | Meta options for quizzes: score, category, lectureID, topic
-setQuizMeta :: Block -> Quiz -> Quiz
-setQuizMeta (CodeBlock (id_, cls, kvs) code) q =
-  if "yaml" `elem` cls
-    then (setCategory . setLectureID) q
-    else q
-  where
-    meta = decodeYaml code
-    quizMetaOptions :: [String]
-    quizMetaOptions = ["category", "score", "lectureID", "topic"]
-    setCategory :: Quiz -> Quiz
-    setCategory q =
-      case getMetaString "category" meta of
-        Just s -> set category (T.pack s) q
-        Nothing -> q
-    -- setScore :: Quiz -> Meta -> Quiz
-    -- setScore q =
-    --   case getMetaString "score" meta of
-    --     Just s -> set score (T.pack s) q
-    --     Nothing -> q
-    setLectureID :: Quiz -> Quiz
-    setLectureID q =
-      case getMetaString "lectureId" meta of
-        Just s -> set qId (T.pack s) q
-        Nothing -> q
-    decodeYaml :: T.Text -> Meta
-    decodeYaml text =
-      case decodeEither' (encodeUtf8 text) of
-        Right a -> toPandocMeta a
-        Left exception -> Meta M.empty
-setQuizMeta _ q = q
+-- renderMultipleChoice :: Quiz -> Block
+-- renderMultipleChoice quiz@(MultipleChoice title tgs cat id_ q ch) =
+--   Div ("", tgs, [("category", cat), ("qID", id_)]) [Para title, choiceBlock]
+--   where
+--     choiceBlock = rawHtml $ T.pack $ S.renderHtml $ choiceList
+--     choiceList = H.ul $ (foldr (\x -> (>>) (handleChoices x)) H.area ch)
+--     handleChoices :: Choice -> Html
+--     handleChoices (Choice correct text comment) =
+--       if correct
+--         then H.li $
+--              A.class_ "correct" $ do
+--                inlinesToHtml' text
+--                H.div ! A.class_ "tooltip" $ blocksToHtml' comment
+--         else H.li $
+--              A.class_ "wrong" $ do
+--                inlinesToHtml' text
+--                H.div ! A.class_ "tooltip" $ blocksToHtml' comment
+-- renderMultipleChoice q =
+--   Div ("", [], []) [Para [Str "ERROR NO MULTIPLE CHOICE"]]
