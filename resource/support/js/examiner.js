3808d163a1f12ccc14c64a5fc29af6e639b6bcae
Henrik Tramberend <henrik.tramberend@beuth-hochschule.de>
2020-09-28 20:36:57 +0200

    Move examiner implementation around
    - examiner-questions: true  -- enables it

--- resource/support/js/examiner.js
+++ resource/support/js/examiner.js
@@ -1,3 +1,5 @@
+export { prepareExaminer };
+
 function gradeMC(exam) {
   let mcAnswer = exam.querySelector("div.exa-mc");
   if (mcAnswer !== null) {
@@ -7,7 +9,7 @@ function gradeMC(exam) {
     let nCorrectChoices = 0;
     let nCorrect = 0;
     let nWrong = 0;
-    for (choice of choices) {
+    for (let choice of choices) {
       let check = choice.querySelector("div.check-box");
       let checkIsCorrect = choice.classList.contains("correct");
       let isChecked = hasAttribute(check, "checked");
@@ -49,7 +51,7 @@ function gradeMA(exam) {
     let nDetails = details.length;
     let nCorrect = 0;
     let nWrong = 0;
-    for (detail of details) {
+    for (let detail of details) {
       let select = detail.querySelector("select");
       let selectedOption = select.value;
       let correctOption = getAttribute(detail, "correct");
@@ -70,61 +72,63 @@ function gradeMA(exam) {
   }
 }
 
-window.addEventListener("load", () => {
-  let exams = document.querySelectorAll("div.exa-quest");
-  for (let exam of exams) {
-    let button = exam.getElementsByTagName("button")[0];
+function prepareExaminer() {
+  window.addEventListener("load", () => {
+    let exams = document.querySelectorAll("div.exa-quest");
+    for (let exam of exams) {
+      let button = exam.getElementsByTagName("button")[0];
 
-    // Multiple choice answer
-    let mcAnswer = exam.querySelector("div.exa-mc");
-    if (mcAnswer !== null) {
-      let choices = mcAnswer.querySelectorAll("div.exa-mc div.choice");
-      for (choice of choices) {
-        let check = choice.querySelector("div.check-box");
-        check.addEventListener("click", e => {
-          if (!hasAttribute(exam, "solved")) {
-            toggleAttribute(e.target, "checked");
+      // Multiple choice answer
+      let mcAnswer = exam.querySelector("div.exa-mc");
+      if (mcAnswer !== null) {
+        let choices = mcAnswer.querySelectorAll("div.exa-mc div.choice");
+        for (let choice of choices) {
+          let check = choice.querySelector("div.check-box");
+          check.addEventListener("click", e => {
+            if (!hasAttribute(exam, "solved")) {
+              toggleAttribute(e.target, "checked");
+            }
+          });
+        }
+        button.addEventListener("click", e => {
+          gradeMC(exam);
+          setAttribute(exam, "solved", true);
+        });
+      }
+
+      // Free form answer
+      let ffAnswer = exam.querySelector("div.exa-ff");
+      if (ffAnswer !== null) {
+        let textarea = ffAnswer.querySelector("textarea");
+        button.addEventListener("click", e => {
+          gradeFF(exam);
+          setAttribute(exam, "solved", true);
+          textarea.setAttribute("readonly", true);
+        });
+      }
+
+      // Multiple answers
+      let maAnswer = exam.querySelector("table.exa-ma");
+      if (maAnswer !== null) {
+        let rows = exam.querySelectorAll("table.exa-ma tr.detail");
+        button.addEventListener("click", e => {
+          gradeMA(exam);
+          setAttribute(exam, "solved", true);
+          for (let row of rows) {
+            let correct = getAttribute(row, "correct");
+            let select = row.querySelector("select");
+            if (select.value === correct) {
+              setAttribute(row, "right", true);
+            } else {
+              setAttribute(row, "wrong", true);
+            }
+            select.setAttribute("disabled", true);
           }
         });
       }
-      button.addEventListener("click", e => {
-        gradeMC(exam);
-        setAttribute(exam, "solved", true);
-      });
     }
-
-    // Free form answer
-    let ffAnswer = exam.querySelector("div.exa-ff");
-    if (ffAnswer !== null) {
-      let textarea = ffAnswer.querySelector("textarea");
-      button.addEventListener("click", e => {
-        gradeFF(exam);
-        setAttribute(exam, "solved", true);
-        textarea.setAttribute("readonly", true);
-      });
-    }
-
-    // Multiple answers
-    let maAnswer = exam.querySelector("table.exa-ma");
-    if (maAnswer !== null) {
-      let rows = exam.querySelectorAll("table.exa-ma tr.detail");
-      button.addEventListener("click", e => {
-        gradeMA(exam);
-        setAttribute(exam, "solved", true);
-        for (row of rows) {
-          let correct = getAttribute(row, "correct");
-          let select = row.querySelector("select");
-          if (select.value === correct) {
-            setAttribute(row, "right", true);
-          } else {
-            setAttribute(row, "wrong", true);
-          }
-          select.setAttribute("disabled", true);
-        }
-      });
-    }
-  }
-});
+  });
+}
 
 function getAttribute(e, attrName) {
   return e.getAttribute("data-" + attrName);
