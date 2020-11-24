505d13dea688f9d44fe808005f7e9a3ca2906ab5
Henrik Tramberend <monofon@tramberend.de>
2020-11-24 07:25:57 +0100

    Fix build setup for water.css

--- third-party/symlinks.mk
+++ third-party/symlinks.mk
@@ -45,8 +45,8 @@ lazyload:
 
 water.css:
 	@mkdir -p $(support)/css
-	(cd water.css && yarn build)
-	@cp $(third)/water.css/out/*.min.css $(support)/css
+	(cd water.css && yarn install)
+	@cp $(third)/water.css/dist/*.min.css $(support)/css
 	chmod a-x $(support)/css/*
 
 fontawesome:
