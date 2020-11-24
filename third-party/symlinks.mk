a40c5261118cfd13d16e4337df47bcec14ed648f
Henrik Tramberend <monofon@tramberend.de>
2020-11-24 17:40:05 +0100

    Increase yarn network-timeout

--- third-party/symlinks.mk
+++ third-party/symlinks.mk
@@ -45,7 +45,7 @@ lazyload:
 
 water.css:
 	@mkdir -p $(support)/css
-	(cd water.css && yarn install)
+	(cd water.css && yarn install --network-timeout 1000000)
 	@cp $(third)/water.css/dist/*.min.css $(support)/css
 	chmod a-x $(support)/css/*
 
