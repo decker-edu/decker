ee61a497056b1fdb0178282f5891bcc741d2cfa9
Mario Botsch <mario.botsch@tu-dortmund.de>
2021-02-04 02:41:32 +0100

    make recording even cooler

--- resource/support/plugins/explain/explain.js
+++ resource/support/plugins/explain/explain.js
@@ -4,18 +4,32 @@
 let ExplainPlugin = (function () {
 
   // GUI elements
-  let recordButton, playButton, player, video, panel;
+  let playPanel, playButton, player;
+  let recordPanel, recordIndicator, voiceIndicator, desktopIndicator;
+  let voiceGainSlider, desktopGainSlider;
 
   // recording stuff
   let blobs;
-  let rec;
+  let recorder;
   let stream;
-  let voiceStream;
-  let desktopStream;
-  let recording = false;
+  let voiceStream, desktopStream;
+  let voiceGain, desktopGain;
+  let volumeMeter;
 
   // playback stuff
   let explainVideoUrl, explainTimes;
+
+
+  // GUI helper (uses named parameters)
+  function createElement({type, id, classes, title, parent}) {
+    let e = document.createElement(type);
+    if (id) e.id = id;
+    if (classes) e.className = classes;
+    if (title) e.title = title;
+    if (parent) parent.appendChild(e);
+    return e;
+  }
+
 
   function deckUrlBase() {
     let path = location.pathname;
@@ -64,7 +78,7 @@ let ExplainPlugin = (function () {
 
 
   function stop() {
-    panel.removeAttribute("data-visible");
+    playPanel.removeAttribute("data-visible");
     player.pause();
     let slide = currentSlide();
     Reveal.slide(slide);
@@ -72,7 +86,7 @@ let ExplainPlugin = (function () {
 
 
   function play() {
-    panel.setAttribute("data-visible", 1);
+    playPanel.setAttribute("data-visible", 1);
 
     let ended = () => {
       player.off("ended", ended);
@@ -94,16 +108,6 @@ let ExplainPlugin = (function () {
   }
 
 
-  function record() {
-    if (!recording) {
-      startRecording();
-      recording = true;
-    }
-    else {
-      stopRecording();
-      recording = false;
-    }
-  }
 
   class Timing {
     constructor() {
@@ -149,16 +153,38 @@ let ExplainPlugin = (function () {
       },
       audio: true
     });
+    if (desktopStream.getAudioTracks().length > 0)
+    {
+      let label = desktopStream.getAudioTracks()[0].label;
+      desktopIndicator.title = label;
+      desktopGainSlider.disabled = false;
+    }
+    else {
+      desktopIndicator.removeAttribute('title');
+      desktopGainSlider.disabled = true;
+    }
+
 
     // get microphone stream
     console.log('get voice stream');
     voiceStream = await navigator.mediaDevices.getUserMedia({
       video: false,
       audio: {
-        echoCancellation: true,
+        echoCancellation: false,
         noiseSuppression: true
       }
     });
+    if (voiceStream.getAudioTracks().length > 0)
+    {
+      let label = voiceStream.getAudioTracks()[0].label;
+      voiceIndicator.title = label;
+      voiceGainSlider.disabled = false;
+    }
+    else {
+      voiceIndicator.removeAttribute('title');
+      voiceGainSlider.disabled = true;
+    }
+
 
     // merge tracks into recording stream
     const tracks = [
@@ -167,9 +193,11 @@ let ExplainPlugin = (function () {
     ];
     stream = new MediaStream(tracks);
 
-    // show record button
-    recordButton.style.display = 'block';
+
+    // show recording panel
+    recordPanel.style.visibility = "visible";
   }
+
 
   function download(blob, name) {
     let url = URL.createObjectURL(blob);
@@ -182,32 +210,31 @@ let ExplainPlugin = (function () {
     document.body.removeChild(anchor);
   }
 
-  function startRecording() {
-    recordButton.title = "Stop video recording";
-    recordButton.classList.remove("fa-circle");
-    recordButton.classList.add("fa-stop");
 
+  function startRecording() {
     // clear blobs array
     blobs = [];
 
     // setup recorder
-    rec = new MediaRecorder(stream, {mimeType: 'video/webm; codecs=h264"'});
+    recorder = new MediaRecorder(stream, {mimeType: 'video/webm; codecs=h264"'});
 
-    rec.ondataavailable = (e) => blobs.push(e.data);
+    recorder.ondataavailable = (e) => blobs.push(e.data);
 
-    rec.onstart = () => {
+    recorder.onstart = () => {
       console.log("[] recorder started")
-      rec.timing = new Timing();
-      rec.timing.start(0, Reveal.getSlides()[0]);
+      recorder.timing = new Timing();
+      recorder.timing.start(0, Reveal.getSlides()[0]);
       Reveal.slide(0);
       Reveal.addEventListener('slidechanged',
-        e => rec.timing.record(e.indexh, e.currentSlide));
+        e => recorder.timing.record(e.indexh, e.currentSlide));
+
+      updateRecordIndicator();
     };
 
-    rec.onstop = async () => {
+    recorder.onstop = async () => {
       console.log("[] recorder stopped")
       let vblob = new Blob(blobs, {type: 'video/webm'});
-      let tblob = rec.timing.finish();
+      let tblob = recorder.timing.finish();
 
       if (
         !await guardedUploadBlob(deckTimesUrl(), tblob) ||
@@ -217,51 +244,376 @@ let ExplainPlugin = (function () {
         download(tblob, videoFilenameBase() + '-times.json');
       }
 
+      recorder = null;
       stream = null;
-      rec = null;
+
+      updateRecordIndicator();
     };
 
-    rec.start();
+    recorder.onpause = updateRecordIndicator;
+    recorder.onresume = updateRecordIndicator;
+
+    recorder.start();
+  }
+
+
+  function pauseRecording() {
+    if (recorder) {
+      switch (recorder.state)
+      {
+        case 'paused':
+          recorder.resume();
+          break;
+        case 'recording':
+          recorder.pause();
+          break;
+      }
+    }
   }
 
 
   function stopRecording() {
-    recordButton.title = "Start video recording";
-    recordButton.classList.remove("fa-stop");
-    recordButton.classList.add("fa-circle");
-
-    rec.stop();
-    stream.getTracks().forEach(s => s.stop())
+    recorder.stop();
+    stream.getTracks().forEach(s => s.stop());
   }
 
 
+  function createAudioMeter(audioContext, meter) {
+
+    let analyser = audioContext.createAnalyser();
+    analyser.fftSize = 512; 
+    analyser.buffer = new Float32Array(analyser.frequencyBinCount);
+    analyser.fade = 0.9;
+    analyser.vol = 0;
+    analyser.db  = 0;
+
+    analyser.draw = function() {
+      // get data from analyser node
+      analyser.getFloatTimeDomainData(analyser.buffer);
+      // compute maximum
+      let vol = analyser.buffer.reduce((previous, current) => { return Math.max(previous, current); });
+      // fade out with previous frame
+      analyser.vol = Math.max(vol, analyser.vol * analyser.fade);
+      // convert to dB, assign to meter
+      analyser.db = 20 * Math.log10( Math.max(0.001, analyser.vol) );
+      meter.value = analyser.db;
+      // draw again next frame
+      window.requestAnimationFrame(analyser.draw);
+    }
+
+    return analyser;
+  }
+
+
+  // adapted from https://paul.kinlan.me/screen-recorderrecording-microphone-and-the-desktop-audio-at-the-same-time/
   const mergeAudioStreams = (desktopStream, voiceStream) => {
     const context = new AudioContext();
     const destination = context.createMediaStreamDestination();
     let hasDesktop = false;
     let hasVoice = false;
+
+    // create audio meter
+    let audioMeter = createAudioMeter(context, volumeMeter);
+
     if (desktopStream && desktopStream.getAudioTracks().length > 0) {
       const source1 = context.createMediaStreamSource(desktopStream);
-      const desktopGain = context.createGain();
+      desktopGain = context.createGain();
       desktopGain.gain.value = 0.7;
-      source1.connect(desktopGain).connect(destination);
+      source1.connect(desktopGain).connect(audioMeter);
+      desktopGain.connect(audioMeter);
       hasDesktop = true;
       console.log("recording desktop audio");
     }
 
     if (voiceStream && voiceStream.getAudioTracks().length > 0) {
       const source2 = context.createMediaStreamSource(voiceStream);
-      const voiceGain = context.createGain();
+      voiceGain = context.createGain();
       voiceGain.gain.value = 1.0;
-      source2.connect(voiceGain).connect(destination);
+      source2.connect(voiceGain).connect(audioMeter);
+      voiceGain.connect(audioMeter);
       hasVoice = true;
       console.log("recording mic audio");
     }
+
+    audioMeter.connect(destination);
+    audioMeter.draw();
 
     return (hasDesktop || hasVoice) ? destination.stream.getAudioTracks() : [];
   };
 
 
+  function createPlayerGUI() {
+    playPanel = createElement({
+      type: "div",
+      id: "dvo-panel",
+      parent: document.body
+    });
+
+    playButton = createElement({
+      type: "button",
+      id: "dvo-play",
+      classes: "dvo-button fas fa-play",
+      title: "Play video recording",
+      parent: document.body
+    });
+    playButton.addEventListener("click", play);
+
+    let video = createElement({
+      type: "video",
+      id: 'dvo-video',
+      classes: "video-js",
+      parent: playPanel
+    });
+
+    // setup video-js
+    player = videojs('dvo-video', {
+      width: "100%",
+      height: "100%",
+      controls: true,
+      autoplay: false,
+      preload: 'metadata',
+      playbackRates: [0.5, 0.75, 1, 1.25, 1.5, 2],
+      controlBar: {
+        playToggle: true,
+        volumePanel: true,
+        currentTimeDisplay: true,
+        timeDivider: false,
+        durationDisplay: false,
+        remainingTimeDisplay: true,
+        playbackRateMenuButton: true,
+        fullscreenToggle: false,
+        pictureInPictureToggle: false
+      },
+      userActions: {
+        hotkeys: function (event) {
+          event.stopPropagation();
+
+          switch (event.code) {
+            // space: play/pause
+            case 'Space':
+              if (this.paused())
+                this.play();
+              else
+                this.pause();
+              break;
+
+            // left/right: skip slides
+            case 'ArrowLeft':
+              prev();
+              break;
+            case 'ArrowRight':
+              next();
+              break;
+
+            // esc: stop and hide video
+            case 'Escape':
+              stop();
+              break;
+
+            // t: record time stamp, print to console
+            case 'KeyT':
+              explainTimes.push(Math.floor(player.currentTime()));
+              printTimeStamps();
+              break;
+          }
+        }
+      }
+    });
+
+    player.on('error', _ => {
+      console.error("ExplainPlugin: Could not open video \"" + explainVideoUrl + "\"");
+      playButton.style.visibility = 'hidden';
+    });
+
+
+    // get videojs button class
+    let Button = videojs.getComponent('Button');
+
+
+    // register and add close button
+    let closeButton = videojs.extend(Button, {
+      constructor: function () {
+        Button.apply(this, arguments);
+        this.addClass('vjs-icon-cancel');
+        this.controlText('Close video');
+      },
+      handleClick: function () { stop(); }
+    });
+    videojs.registerComponent('closeButton', closeButton);
+    player.getChild('controlBar').addChild('closeButton', {}, 0);
+
+
+    // register and add prev button
+    let prevButton = videojs.extend(Button, {
+      constructor: function () {
+        Button.apply(this, arguments);
+        this.addClass('vjs-icon-previous-item');
+        this.controlText('Jump to previous slide');
+      },
+      handleClick: function () { prev(); }
+    });
+    videojs.registerComponent('prevButton', prevButton);
+    player.getChild('controlBar').addChild('prevButton', {}, 1);
+
+
+    // register and add next button
+    let nextButton = videojs.extend(Button, {
+      constructor: function () {
+        Button.apply(this, arguments);
+        this.addClass('vjs-icon-next-item');
+        this.controlText('Jump to next slide');
+      },
+      handleClick: function () { next(); }
+    });
+    videojs.registerComponent('nextButton', nextButton);
+    player.getChild('controlBar').addChild('nextButton', {}, 3);
+  }
+
+
+  function createRecordingGUI() 
+  {
+    recordPanel = createElement({ 
+      type: "div", 
+      id: "record-panel", 
+      parent: document.body 
+    });
+    recordPanel.style.visibility = "hidden";
+
+
+    let row;
+    row = createElement({
+      type: "div",
+      classes: "controls-row",
+      parent: recordPanel
+    });
+
+    recordIndicator = createElement({
+      type: "i", 
+      id: "record-indicator", 
+      classes: "indicator", 
+      parent: row
+    });
+
+    volumeMeter = createElement({
+      type: "meter", 
+      id: "audio-meter", 
+      parent: row
+    });
+    volumeMeter.value = -60;
+    volumeMeter.min  = -60;
+    volumeMeter.low  = -20;
+    volumeMeter.high = -9;
+    volumeMeter.max  = 0;
+
+
+    let controls = createElement({
+      type: "div", 
+      id: "record-controls", 
+      parent: recordPanel
+    });
+    
+    row = createElement({
+      type: "div",
+      classes: "controls-row",
+      parent: controls
+    });
+
+    let recordButton = createElement({
+      type: "button", 
+      id: "record-start", 
+      classes: "dvo-button fas fa-play-circle", 
+      title: "Start recording",
+      parent: row
+    });
+    recordButton.addEventListener("click", startRecording);
+
+    let pauseButton = createElement({
+      type: "button", 
+      id: "record-stop", 
+      classes: "dvo-button fas fa-pause-circle",
+      title: "Pause/resume recording",
+      parent: row
+    });
+    pauseButton.addEventListener("click", pauseRecording);
+
+    let stopButton = createElement({
+      type: "button", 
+      id: "record-stop", 
+      classes: "dvo-button fas fa-stop-circle",
+      title: "Stop recording",
+      parent: row
+    });
+    stopButton.addEventListener("click", stopRecording);
+
+
+    row = createElement({
+      type: "div",
+      classes: "controls-row",
+      parent: controls
+    });
+
+    voiceIndicator = createElement({
+      type: "i", 
+      id: "voice-indicator", 
+      classes: "indicator", 
+      parent: row
+    });
+
+    voiceGainSlider = createElement({
+      type: "input",
+      id: "voice-gain-slider", 
+      title: "Microphone Audio Gain",
+      parent: row
+    });
+    voiceGainSlider.type = "range";
+    voiceGainSlider.min = 0;
+    voiceGainSlider.max = 2;
+    voiceGainSlider.value = 1;
+    voiceGainSlider.step = 0.1;
+    voiceGainSlider.oninput = () => { 
+      if (voiceGain) {
+        voiceGain.gain.value = voiceGainSlider.value;
+        console.log(voiceGain.gain.value);
+      }
+    };
+
+
+    row = createElement({
+      type: "div",
+      classes: "controls-row",
+      parent: controls
+    });
+
+    desktopIndicator = createElement({
+      type: "i", 
+      id: "desktop-indicator", 
+      classes: "indicator", 
+      parent: row
+    });
+
+    desktopGainSlider = createElement({
+      type: "input", 
+      id: "desktop-gain-slider",
+      title: "Desktop Audio Gain",
+      parent: row
+    });
+    desktopGainSlider.type = "range";
+    desktopGainSlider.min = 0;
+    desktopGainSlider.max = 2;
+    desktopGainSlider.value = 1;
+    desktopGainSlider.step = 0.1;
+    desktopGainSlider.oninput = () => { 
+      if (desktopGain) {
+        desktopGain.gain.value = desktopGainSlider.value;
+        console.log(desktopGain.gain.value);
+      }
+    };
+  }
+
+
+  function updateRecordIndicator() {
+    recordIndicator.dataset.state = recorder ? recorder.state : '';
+  }
 
   function printTimeStamps() {
     for (let i = 0; i < explainTimes.length; i++) {
@@ -341,138 +693,12 @@ let ExplainPlugin = (function () {
         console.log("[] no explanation source");
       }
 
-      recordButton = document.createElement("button");
-      recordButton.id = "dvo-record";
-      recordButton.classList.add("dvo-button", "fas", "fa-circle");
-      recordButton.title = "Start video recording";
-      recordButton.addEventListener("click", record);
-      document.body.appendChild(recordButton);
-      recordButton.style.display = 'none';
-
-      playButton = document.createElement("button");
-      playButton.id = "dvo-play";
-      playButton.classList.add("dvo-button", "fas", "fa-play");
-      playButton.addEventListener("click", play);
-      playButton.title = "Play video recording";
-      document.body.appendChild(playButton);
-
-      panel = document.createElement("div");
-      panel.id = "dvo-panel";
-      document.body.appendChild(panel);
-
-      let video = document.createElement("video");
-      video.id = 'dvo-video';
-      video.classList.add('video-js');
-      panel.appendChild(video);
-
-      // setup video-js
-      player = videojs('dvo-video', {
-        width: "100%",
-        height: "100%",
-        controls: true,
-        autoplay: false,
-        preload: 'metadata',
-        playbackRates: [0.5, 0.75, 1, 1.25, 1.5, 2],
-        controlBar: {
-          playToggle: true,
-          volumePanel: true,
-          currentTimeDisplay: true,
-          timeDivider: false,
-          durationDisplay: false,
-          remainingTimeDisplay: true,
-          playbackRateMenuButton: true,
-          fullscreenToggle: false,
-          pictureInPictureToggle: false
-        },
-        userActions: {
-          hotkeys: function(event) {
-            event.stopPropagation();
-
-            switch (event.code)
-            {
-              // space: play/pause
-              case 'Space':
-                if (this.paused())
-                  this.play();
-                else
-                  this.pause();
-                break;
-
-              // left/right: skip slides
-              case 'ArrowLeft':
-                prev();
-                break;
-              case 'ArrowRight':
-                next();
-                break;
-
-              // esc: stop and hide video
-              case 'Escape':
-                stop();
-                break;
-
-              // t: record time stamp, print to console
-              case 'KeyT':
-                explainTimes.push(Math.floor(player.currentTime()));
-                printTimeStamps();
-                break;
-            }
-          }
-        }
-      });
-
-      player.on('error', _ => {
-        console.error("ExplainPlugin: Could not open video \"" + explainVideoUrl + "\"");
-        playButton.style.visibility = 'hidden';
-      });
-
-
-      // get videojs button class
-      let Button = videojs.getComponent('Button');
-
-      // register and add close button
-      let closeButton = videojs.extend(Button, {
-        constructor: function() {
-          Button.apply(this, arguments);
-          this.addClass('vjs-icon-cancel');
-          this.controlText('Close video');
-        },
-        handleClick: function() { stop(); }
-      });
-      videojs.registerComponent('closeButton', closeButton);
-      player.getChild('controlBar').addChild('closeButton', {}, 0);
-
-
-      // register and add prev button
-      let prevButton = videojs.extend(Button, {
-        constructor: function() {
-          Button.apply(this, arguments);
-          this.addClass('vjs-icon-previous-item');
-          this.controlText('Jump to previous slide');
-        },
-        handleClick: function() { prev(); }
-      });
-      videojs.registerComponent('prevButton', prevButton);
-      player.getChild('controlBar').addChild('prevButton', {}, 1);
-
-
-      // register and add next button
-      let nextButton = videojs.extend(Button, {
-        constructor: function() {
-          Button.apply(this, arguments);
-          this.addClass('vjs-icon-next-item');
-          this.controlText('Jump to next slide');
-        },
-        handleClick: function() { next(); }
-      });
-      videojs.registerComponent('nextButton', nextButton);
-      player.getChild('controlBar').addChild('nextButton', {}, 3);
-
+      createRecordingGUI();
+      createPlayerGUI();
 
 
       // if we have a video, use it
       if (explainVideoUrl && explainTimes) {
-        recordButton.style.display = 'none';
         player.src({type: 'video/mp4', src: explainVideoUrl});
       }
       // otherwise let's record one
