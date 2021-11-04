6b2b5f5c42f03a54bc48c913ea1e8f6afd1392e2
Sebastian Lukas Hauer <sebastian.hauer@udo.edu>
2021-11-04 16:30:00 +0100

    added view-distance 1 so invisible links are not focusable and changed the API to represent what it actually does

--- resource/decker/support/plugins/decker/plugin-manager.js
+++ resource/decker/support/plugins/decker/plugin-manager.js
@@ -1,22 +1,23 @@
 /**
  * A plugin that manages other plugins.
- * In order to facilitate an accessible user interface it is paramount that the tab-order of its
- * elements is in reading order. Different plugins add different interactible elements into the DOM
- * in an arbitrary order. This plugin exists to allow developers of plugins to add buttons to the
- * screen in a way that does not break the tab-order of their onscreen elements.
+ * In order to facilitate an accessible user interface it is important that the tab-order of its
+ * elements is in a sensible order. Different plugins add different interactible elements into
+ * the DOM in an arbitrary order. This plugin exists to allow developers of plugins to add buttons
+ * to the screen in a way that does not break the tab-order of their onscreen elements.
  * 
  * To add an element to one of the four (three if the bottom right navigation is to be left alone) anchors,
- * simply register a plugin with the registerPlugin(...) method.
+ * simply place a button with the placeButton(...) method.
  * 
- * The registered plugin object needs two attributes:
- * decker_button: The DOM element that should be added to an anchor.
- * decker_anchor: The anchor the DOM element should be added to: TOP_LEFT, TOP_RIGHT, BOTTOM_LEFT or BOTTOM_RIGHT.
+ * In order to place a button you need to specify two things:
+ * button: The DOM element that should be added to an anchor.
+ * position: The anchor the DOM element should be added to: TOP_LEFT, TOP_RIGHT, BOTTOM_LEFT or BOTTOM_RIGHT.
  * 
- * In addition, the plugins.css stylesheet allows for modification of the general look and feel of all buttons added this way.
+ * In addition, the plugins.css stylesheet allows for modification of the general look and feel of
+ * all buttons added this way.
  * TODO: More responsive css for mobile users?
  */
 
-class DeckerPlugins {
+ class DeckerPlugins {
     id = "decker-plugins";
     reveal = undefined; //reference to the reveal object received in init
     top_left_anchor = undefined;
@@ -33,18 +34,17 @@ class DeckerPlugins {
         template.innerHTML = String.raw
         `<div class="${classname} decker-anchor">
         </div>`;
-        let div = template.content.firstChild;
+        let div = template.content.firstElementChild;
         return div;
     }
 
-    registerPlugin(plugin) {
-        let button = plugin.decker_button;
-        let anchor = plugin.decker_anchor;
-        switch(anchor) {
+    placeButton(button, position) {
+        switch(position) {
             case "TOP_LEFT": this.addTopLeftButton(button); break;
             case "TOP_RIGHT": this.addTopRightButton(button); break;
             case "BOTTOM_LEFT": this.addBottomLeftButton(button); break;
-            default: console.log("[PLUGIN MANAGER] [ERROR] No valid anchor specified.");
+            case "BOTTOM_RIGHT": this.addBottomRightButton(button); break;
+            default: console.error("[PLUGIN MANAGER] [ERROR] No valid anchor specified.");
         }
     }
 
@@ -58,6 +58,10 @@ class DeckerPlugins {
 
     addBottomLeftButton(button) {
         this.bottom_left_anchor.appendChild(button);
+    }
+
+    addBottomRightButton(button) {
+        this.bottom_right_anchor.appendChild(button);
     }
 
     init(reveal) {
