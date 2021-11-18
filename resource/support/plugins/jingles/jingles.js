ba64f64f50f9cf4929d487643009689791dc896e
Mario Botsch <mario.botsch@tu-dortmund.de>
2021-11-18 21:34:16 +0100

    change jingles hotkey

--- resource/support/plugins/jingles/jingles.js
+++ resource/support/plugins/jingles/jingles.js
@@ -1,23 +1,29 @@
 "use strict";
 
 let RevealJingle = (function () {
-
   const config = Reveal.getConfig().jingles || {};
   let jingleLayout = null;
   let jingles = [];
 
-
   function createJingle(filename, style, volume) {
-    console.log("  add jingle " + filename + " with volume " + (volume?volume:"1.0"));
+    console.log(
+      "  add jingle " + filename + " with volume " + (volume ? volume : "1.0")
+    );
 
-    const defaultStyle = "display:none; position:absolute; left:0; right:0; top:0; bottom:0; margin:auto; object-fit:contain; pointer-events:none;"
+    const defaultStyle =
+      "display:none; position:absolute; left:0; right:0; top:0; bottom:0; margin:auto; object-fit:contain; pointer-events:none;";
 
     let vid = document.createElement("video");
-    vid.style = style ? defaultStyle+style : defaultStyle;
+    vid.style = style ? defaultStyle + style : defaultStyle;
     if (volume) vid.volume = volume;
     vid.src = filename;
-    vid.onplay = () => { jingleLayout.style.display = "flex"; vid.style.display = "block"; }
-    vid.onended = () => { jingleLayout.style.display = vid.style.display = "none"; }
+    vid.onplay = () => {
+      jingleLayout.style.display = "flex";
+      vid.style.display = "block";
+    };
+    vid.onended = vid.onpause = () => {
+      jingleLayout.style.display = vid.style.display = "none";
+    };
     jingleLayout.appendChild(vid);
 
     return vid;
@@ -27,33 +33,60 @@ let RevealJingle = (function () {
     // setup container
     const reveal = document.querySelector(".reveal");
     jingleLayout = document.createElement("div");
-    jingleLayout.style = "display:none; flex-flow: column nowrap; justify-content:center; position:fixed; left:0; right:0; top:0; bottom:0; margin:0; z-index:40; background:none; pointer-events:none;";
+    jingleLayout.style =
+      "display:none; flex-flow: column nowrap; justify-content:center; position:fixed; left:0; right:0; top:0; bottom:0; margin:0; z-index:40; background:none; pointer-events:none;";
     reveal.appendChild(jingleLayout);
 
     // setup video element per jingle
-    for (let i=0; i<config.length; i++) {
-      jingles.push( createJingle(config[i].vid, config[i].css, config[i].vol) );
+    for (let i = 0; i < config.length; i++) {
+      jingles.push(createJingle(config[i].vid, config[i].css, config[i].vol));
     }
   }
 
   function playJingle(i) {
-    jingles[i].play();
+    for (let j = 0; j < jingles.length; j++) if (j != i) jingles[j].pause();
+    if (i < jingles.length) {
+      jingles[i].currentTime = 0;
+      jingles[i].play();
+    }
   }
 
   function setupKeyBindings() {
-    const N = Math.min(9, jingles.length);
-    for (let i = 0; i < N; i++) {
-      Reveal.addKeyBinding(
-        {
-          keyCode: 49 + i,
-          key: String.fromCharCode(49 + i),
-          description: "Play jingle " + (i+1),
-        },
-        () => {
-          playJingle(i);
+    window.addEventListener("keydown", function (evt) {
+      if (evt.shiftKey) {
+        switch (evt.code) {
+          case "Digit1":
+            playJingle(0);
+            evt.preventDefault();
+            evt.stopPropagation();
+            break;
+
+          case "Digit2":
+            playJingle(1);
+            evt.preventDefault();
+            evt.stopPropagation();
+            break;
+
+          case "Digit3":
+            playJingle(2);
+            evt.preventDefault();
+            evt.stopPropagation();
+            break;
+
+          case "Digit4":
+            playJingle(3);
+            evt.preventDefault();
+            evt.stopPropagation();
+            break;
+
+          case "Digit5":
+            playJingle(4);
+            evt.preventDefault();
+            evt.stopPropagation();
+            break;
         }
-      );
-    }
+      }
+    });
   }
 
   return {
