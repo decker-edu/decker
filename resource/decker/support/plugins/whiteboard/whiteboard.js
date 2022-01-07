166a7da8dbbf04c4285ba199c74d8ee56359054f
Sebastian Lukas Hauer <sebastian.hauer@udo.edu>
2022-01-07 14:29:31 +0100

    fixed broken merge from previous commits

--- resource/decker/support/plugins/whiteboard/whiteboard.js
+++ resource/decker/support/plugins/whiteboard/whiteboard.js
@@ -183,11 +183,13 @@ function createGUI() {
     hoverTimer = setInterval(hidePanel, 1000);
   };
 
+  /* As this is no longer the default button the only thing this button now does is exiting
+   * the whiteboard mode. */
   buttonWhiteboard = createButton(
-    "whiteboard decker-button fas fa-edit checkbox",
+    "whiteboard fas fa-edit",
     toggleWhiteboard,
     false,
-    "Whiteboard Menu"
+    "Toggle Whiteboard Mode"
   );
   buttonWhiteboard.id = "whiteboardButton";
 
@@ -226,16 +228,17 @@ function createGUI() {
     () => {
       if (!buttons.classList.contains("visible")) {
         showPanel();
+        buttonPen.focus();
         return;
       }
       if (tool != PEN) {
         selectTool(PEN);
       } else {
-        colorPicker.classList.toggle("active");
+        toggleColorPicker();
       }
     },
     false,
-    "Change Pen"
+    "Select or change Pen"
   );
   buttonPen.setAttribute("role", "switch");
 
@@ -243,7 +246,8 @@ function createGUI() {
     "whiteboard fas fa-eraser radiobutton",
     () => {
       if (!buttons.classList.contains("visible")) {
-        buttons.classList.add("visible");
+        showPanel();
+        buttonEraser.focus();
         return;
       }
       selectTool(ERASER);
@@ -257,7 +261,8 @@ function createGUI() {
     "whiteboard fas fa-magic radiobutton",
     () => {
       if (!buttons.classList.contains("visible")) {
-        buttons.classList.add("visible");
+        showPanel();
+        buttonLaser.focus();
         return;
       }
       selectTool(LASER);
@@ -289,8 +294,11 @@ function createGUI() {
     const slideScale = Reveal.getScale();
     const radius = r * slideScale + "px";
     let b = document.createElement("button");
-    b.className = "whiteboard fas fa-circle";
-    b.style.fontSize = radius;
+    let s = document.createElement("span");
+    s.style.fontSize = radius;
+    s.className = "fas fa-circle";
+    b.className = "whiteboard";
+    b.appendChild(s);
     b.onclick = () => {
       selectPenRadius(r);
     };
@@ -563,8 +571,8 @@ function toggleLaser() {
   else selectTool(LASER);
 }
 
-function showColorPicker() {
-  colorPicker.classList.add("active");
+function toggleColorPicker() {
+  colorPicker.classList.toggle("active");
 }
 
 function hideColorPicker() {
@@ -608,8 +616,10 @@ function toggleWhiteboard(state) {
     // reset cursor
     clearTimeout(hideCursorTimeout);
     slides.style.cursor = "";
+    
   } else {
     if (userShouldBeWarned && !userHasBeenWarned) warnUser();
+    showPanel();
 
     // show scrollbar
     slides.classList.add("active");
