3150d85e07057267585f03b8c6184f2f532927e2
Monofon <monofon@tramberend.de>
2022-02-17 07:36:04 +0100

    Move things around

--- resource/decker/support/plugins/examiner/poll.js
+++ resource/decker/support/plugins/examiner/poll.js
@@ -1,10 +1,22 @@
+// Manages a persistent connection to a polling server.
 export { pollSession };
 
+// This might be overkill, but it is the only generator in a proper ES6 module i
+// could find.
 import bwipjs from "https://cdnjs.cloudflare.com/ajax/libs/bwip-js/3.0.4/bwip-js.mjs";
 
 var session = null;
 
-function pollSession(serverUrl, clientUrl, onready = null, onclose = null) {
+// Creates a new polling session. Opens a web-socket connection to serverUrl and
+// uses clientUrl as the polling client. If clientUrl is null the client
+// provided by the server is used. onready is called when the session is
+// established, onclose when the server terminates the session.
+function pollSession(
+  serverUrl,
+  clientUrl = null,
+  onready = null,
+  onclose = null
+) {
   session = {
     id: null,
     clientUrl: null,
@@ -40,21 +52,34 @@ function pollSession(serverUrl, clientUrl, onready = null, onclose = null) {
         session.clientUrl = `${clientUrl}#${session.id}`;
 
         resolve({
+          // Returns the 4 digit session id and the client url for this session.
           sessionId: () => {
             return { id: session.id, url: session.clientUrl };
           },
 
+          // Constructs a qr code from the client url in the provided canvas element.
           fillQRCode: (canvas) => {
             bwipjs.toCanvas(canvas, {
               bcid: "qrcode", // Barcode type
               text: session.clientUrl, // Text to encode
               scale: 20, // Somehow gives 1000^2 pixels
-              includetext: false, // Show human-readable text
-              textxalign: "center", // Always good to set this
-              eclevel: "L",
+              includetext: false,
+              textxalign: "center",
+              eclevel: "L", // Makes for smaller qr code sizes
             });
           },
 
+          // Starts a new poll. choices is an array of strings that signify the
+          // choices the clients have. Each entry results in a button on the
+          // client. Example: ["A", "B", "C"]. votes is the number of choices
+          // the client should be able to select. Callbacks is an object
+          // containing two callbacks, onActive and onFinished.
+          // onActive(participants, votes) is called whenever the state of an
+          // active poll changes, where participants is the number of clients in
+          // this session and a votes is an object containing the number of
+          // votes for each choice in the active poll. Example: {25, ["A": 12,
+          // "B": 9]}. onFinished(participants, votes) is called with the final
+          // results when the poll has stopped.
           poll: (choices, votes, callbacks) => {
             session.ui = callbacks;
             session.socket.send(
@@ -62,10 +87,9 @@ function pollSession(serverUrl, clientUrl, onready = null, onclose = null) {
             );
           },
 
+          // Terminates the active poll.
           stop: () => {
-            session.socket.send(
-              JSON.stringify({ tag: "Stop", choices: ["ignored", "anyways"] })
-            );
+            session.socket.send(JSON.stringify({ tag: "Stop" }));
           },
         });
       } else {
