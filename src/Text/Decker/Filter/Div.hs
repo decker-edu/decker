29175d2d092e8a0756b601b5187eb7ef3d9f8b3e
Monofon <monofon@tramberend.de>
2022-09-10 18:09:31 +0200

    Cleanup

--- src/Text/Decker/Filter/Div.hs
+++ src/Text/Decker/Filter/Div.hs
@@ -19,35 +19,32 @@ divBasedLayout (Slide header body dir) =
   return (Slide header (map (processColumns . processSize) body) dir)
   where
     -- Finds the first columns tag and remove it from the classes list
-    processColumns div@(Div (id, cls, kvs) body) =
-      let colCls = partition (=~ ("^columns(-[0-9]+)+$" :: String)) cls
-       in case first (find (not . Text.null)) colCls of
-            (Just cols, cls) ->
+    processColumns div@(Div (id, cls, attribs) body) =
+      let columnClasses = partition (=~ ("^columns(-[0-9]+)+$" :: String)) cls
+       in case first (find (not . Text.null)) columnClasses of
+            (Just columnClass, cls) ->
               -- Extracts the numbers from the columns tag, default is 1
-              let frs = map (fromMaybe 1 . readMaybe) $ drop 1 $ splitOn "-" $ toString cols
-                  cls' = ".grid-layout" : cls
+              let columRatios = map (fromMaybe 1 . readMaybe) $ drop 1 $ splitOn "-" $ toString columnClass
+                  gridClasses = ".grid-layout" : cls
                   -- Construct the CSS style
-                  gtc = Text.intercalate " " $ "grid-template-columns:" : map ((<> "fr") . show) frs
-               in -- Final assembly
-                  Div (id, cls', addToStyle kvs [gtc]) body
+                  gridCss = Text.intercalate " " $ "grid-template-columns:" : map ((<> "fr") . show) columRatios
+               in Div (id, gridClasses, addToStyle [gridCss] attribs) body
             _ -> div
     processColumns block = block
     -- Converts size attributes (currently with, height) to CSS style settings
-    processSize div@(Div (id, cls, kvs) body) =
-      let (sizes, rest) = partition ((`elem` ["width", "height"]) . fst) kvs
-          sizesCss = map (\(k, v) -> k <> ":" <> v) sizes
-       in Div (id, "sized" : cls, addToStyle rest sizesCss) body
+    processSize div@(Div (id, cls, attribs) body) =
+      let (sizeAttribs, otherAttribs) = partition ((`elem` ["width", "height"]) . fst) attribs
+          sizesCss = map (\(k, v) -> k <> ":" <> v) sizeAttribs
+       in Div (id, "sized" : cls, addToStyle sizesCss otherAttribs) body
     processSize block = block
     -- Adds a singe CSS style in front of the style attribute. Collapses style
     -- attributes if there are more than just one and preserves already existing
     -- style settings.
-    addToStyle kvs css =
-      let (sty, rest) = partition ((== "style") . fst) kvs
-          sty' =
-            (<> ";") $
-              Text.intercalate ";" $
-                css
-                  <> concatMap
-                    (filter (not . Text.null) . Text.splitOn ";" . snd)
-                    sty
-       in ("style", sty') : rest
+    addToStyle css attribs =
+      let (styleAttribs, otherAttribs) = partition ((== "style") . fst) attribs
+          styleCss =
+            concatMap
+              (filter (not . Text.null) . Text.splitOn ";" . snd)
+              styleAttribs
+          newStyles = (<> ";") $ Text.intercalate ";" $ css <> styleCss
+       in ("style", newStyles) : otherAttribs
