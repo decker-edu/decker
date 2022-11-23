a2271b1d3ba436994a44b64dcd00cb36a3474785
Mario Botsch <mario.botsch@tu-dortmund.de>
2022-11-23 15:40:55 +0100

    add geogebra to support

---
+++ resource/decker/support/geogebra/HTML5/5.0/web/deferredjs/98230ABB1F8D0FE45BF1253554CEF164/3.cache.js
@@ -0,0 +1,1 @@
+$wnd.web.runAsyncCallback3('Pxc(3435,1,Yqm);_.se=function(){};BDl(li)(3);\n//# sourceURL=web-3.js\n')
---
+++ resource/decker/support/geogebra/HTML5/5.0/webSimple/deferredjs/10EE133F2EA69455540DE4DD746AA6EF/2.cache.js
@@ -0,0 +1,1 @@
+$wnd.webSimple.runAsyncCallback2('ozb(1939,1,Rxh);_.Ed=function(){};oZg(Ng)(2);\n//# sourceURL=webSimple-2.js\n')
