201a42d3dfcb2bf9f212f86f36ba8a9b454b8c3a
Monofon <monofon@tramberend.de>
2024-09-16 13:17:08 +0200

    fix macro- and fragment-template handling

--- src/Text/Decker/Filter/FragmentTemplate.hs
+++ src/Text/Decker/Filter/FragmentTemplate.hs
@@ -3,37 +3,38 @@
 
 module Text.Decker.Filter.FragmentTemplate (expandFragmentTemplates) where
 
-import Data.List qualified as List
-import Data.Text qualified as Text
-import Relude
-import Text.Decker.Filter.Monad (Filter, meta, FilterState (templates))
-import Text.Decker.Filter.Util (randomId)
-import Text.Decker.Internal.Meta (lookupMetaOrElse )
-import Text.Decker.Internal.Exception (DeckerException (..))
-import Text.Pandoc
-    ( Inline(RawInline, Link),
-      Block(RawBlock, Para, Plain, CodeBlock),
-      Pandoc(..),
-      renderTemplate,
-      Template )
-import Text.Pandoc.Shared ( stringify )
-import Text.Pandoc.Walk ( Walkable(walkM) )
-import Text.Regex.TDFA.Text ()
-import Text.Decker.Internal.URI (makeProjectPath)
-import Text.DocTemplates (compileTemplateFile, toContext, Context)
-import Data.Map.Strict qualified as Map
-import Control.Exception (throw)
 import Control.Concurrent.STM (modifyTVar)
-import Data.Aeson (toJSON)
-import System.FilePath ((<.>))
-import Text.DocLayout ( render )
-import Text.Decker.Internal.Common (supportDir)
-import Development.Shake.FilePath ((</>))
+import Control.Exception (throw)
 import Control.Exception.Base (handle)
+import Data.Aeson (toJSON)
+import Data.List qualified as List
+import Data.Map.Strict qualified as Map
+import Data.Text qualified as Text
+import Development.Shake.FilePath ((</>))
+import Relude
+import System.FilePath ((<.>))
 import Text.Decker.Filter.Local (needFile)
+import Text.Decker.Filter.Monad (Filter, FilterState (templates), meta)
+import Text.Decker.Filter.Util (randomId)
+import Text.Decker.Internal.Common (projectDir, supportDir)
+import Text.Decker.Internal.Exception (DeckerException (..))
+import Text.Decker.Internal.Meta (lookupMetaOrElse)
+import Text.Decker.Internal.URI (makeProjectPath)
+import Text.DocLayout (render)
+import Text.DocTemplates (Context, compileTemplateFile, toContext)
+import Text.Pandoc
+  ( Block (CodeBlock, Para, Plain, RawBlock),
+    Inline (Link, RawInline),
+    Pandoc (..),
+    Template,
+    renderTemplate,
+  )
+import Text.Pandoc.Shared (stringify)
+import Text.Pandoc.Walk (Walkable (walkM))
+import Text.Regex.TDFA.Text ()
 
 expandFragmentTemplates :: Pandoc -> Filter Pandoc
-expandFragmentTemplates  document@(Pandoc meta blocks) =
+expandFragmentTemplates document@(Pandoc meta blocks) =
   if lookupMetaOrElse False "experiments.fragment-templates" meta
     then Pandoc meta <$> (walkM expandBlockM blocks >>= walkM expandLinkM)
     else return document
@@ -57,14 +58,14 @@ expandFragmentTemplates  document@(Pandoc meta blocks) =
         Just (name, args) -> do
           let kvAttribs = List.filter ((/= "micro") . fst) rawKvs
           let targetArgs = [("url", url), ("title", title)]
-          let posArgs = zip (map (("arg" <>) . show ) [1 .. (length args)]) args
+          let posArgs = zip (map (("arg" <>) . show) [1 .. (length args)]) args
           let allPosArgs = [("args", Text.unwords args)]
-          let clsArgs = zip (map (("class" <>) . show ) [1 .. (length cls)]) cls
+          let clsArgs = zip (map (("class" <>) . show) [1 .. (length cls)]) cls
           let allClsArgs = [("classes", Text.unwords cls)]
           let allKvAttribs = [("attribs", unwords $ map (\(k, v) -> k <> "=\"" <> v <> "\"") kvAttribs)]
           rndId <- liftIO randomId
           let idArg = [("id", if Text.null id then rndId else id)]
-          let arguments :: [(Text,Text)] = allPosArgs <> posArgs <> targetArgs <> idArg <> clsArgs <> allClsArgs <> kvAttribs <> allKvAttribs
+          let arguments :: [(Text, Text)] = allPosArgs <> posArgs <> targetArgs <> idArg <> clsArgs <> allClsArgs <> kvAttribs <> allKvAttribs
           template <- getTemplate (toString name)
           let context :: Context Text = toContext $ toJSON $ Map.fromList arguments
           let text :: Text = render Nothing $ renderTemplate template context
@@ -80,14 +81,14 @@ expandFragmentTemplates  document@(Pandoc meta blocks) =
         Just name -> do
           rndId <- ("id" <>) <$> liftIO randomId
           let kvAttribs = List.filter ((/= "micro") . fst) rawKvs
-          let clsArgs = zip (map (("class" <>) . show ) [1 .. (length cls)]) cls
+          let clsArgs = zip (map (("class" <>) . show) [1 .. (length cls)]) cls
           let allClsArgs = [("classes", Text.unwords cls)]
           let allKvAttribs = [("attribs", unwords $ map (\(k, v) -> k <> "=\"" <> v <> "\"") kvAttribs)]
           let codeArg = [("code", Text.strip code)]
           let rndIdArg = [("rnd-id", rndId)]
           let idArg = [("id", if Text.null id then rndId else id)]
           let captionArg = [("caption", fromMaybe "" (List.lookup "caption" rawKvs))]
-          let arguments :: [(Text,Text)] = codeArg <> clsArgs <> allClsArgs <> kvAttribs <> allKvAttribs <> rndIdArg <> idArg <> captionArg
+          let arguments :: [(Text, Text)] = codeArg <> clsArgs <> allClsArgs <> kvAttribs <> allKvAttribs <> rndIdArg <> idArg <> captionArg
           template <- getTemplate (toString name)
           let context :: Context Text = toContext $ toJSON $ Map.fromList arguments
           let text :: Text = render Nothing $ renderTemplate template context
@@ -113,7 +114,7 @@ getTemplate filename = do
   case Map.lookup filename templates of
     Nothing -> do
       template <- do
-        readTemplateFile (filename  <.> "html")
+        readTemplateFile (filename <.> "html")
       atomically $ modifyTVar tvar (Map.insert filename template)
       return template
     Just template -> return template
@@ -123,25 +124,36 @@ readTemplateFile filename = do
   meta <- gets meta
   let base :: String = lookupMetaOrElse "." "decker.base-dir" meta
   let path1 = makeProjectPath base filename
-  let path2 = supportDir </> "templates" </> filename
+  let path2 = projectDir </> "templates" </> filename
+  let path3 = supportDir </> "templates" </> filename
 
   -- compileTemplateFile throws exceptions if the file data can not be read, and
   -- returns (Left (Template a)) if the template cannot be compiled. this code
   -- tries two template locations in order and tries the second one only if the
-  -- first one cannot be found. if the template cannot be found ist throws on
+  -- first one cannot be found. if the template cannot be found it throws an
   -- either.
-  (path, template) <- liftIO $
-    handle (\(SomeException _) ->
-      handle (\(SomeException err)->
-                throw (ResourceException $ "Cannot find template file: " <> filename <> ": " <> show err))
-             $ compileTemplate path2) $ compileTemplate path1
+  (path, template) <-
+    liftIO
+      $ handle
+        ( \(SomeException _) ->
+            handle
+              ( \(SomeException _) ->
+                  handle
+                    ( \(SomeException err) ->
+                        throw (ResourceException $ "Cannot find template file: " <> filename <> ": " <> show err)
+                    )
+                    $ compileTemplate path3
+              )
+              $ compileTemplate path2
+        )
+      $ compileTemplate path1
   case template of
     Right template -> do
-        needFile path
-        return template
+      needFile path
+      return template
     Left err -> do
       return $ throw (ResourceException $ "Cannot parse template file: " <> filename <> ": " <> show err)
-  where 
+  where
     compileTemplate path = do
-        t <- compileTemplateFile path
-        return (path, t)
+      t <- compileTemplateFile path
+      return (path, t)
