c59dd9ffe44c08a5b5ae5811b52fa58f76d0ada8
Monofon <monofon@tramberend.de>
2025-01-16 09:15:06 +0100

    stop throwing on missing decker.yaml for commands

--- src/Text/Decker/Project/Shake.hs
+++ src/Text/Decker/Project/Shake.hs
@@ -76,19 +76,14 @@ runDeckerArgs args theRules = do
         if null targets
           then theRules
           else want targets >> withoutActions theRules
-  deckerMeta <- readMetaDataFile deckerMetaFile
-  case deckerMeta of
-    Right meta -> do
-      context <- initContext flags meta
-      let commands = ["clean", "purge", "example", "serve", "crunch", "crrrunch", "transcribe", "transcrrribe", "pdf", "version", "check", "format"]
-      case targets of
-        [command] | command `elem` commands -> runCommand context command rules
-        _ -> do
-          warnVersion
-          runTargets context targets rules
-    Left err -> do
-      putStrLn "ERROR: cannot find `decker.yaml`"
-      exitFailure
+  meta <- fromRight nullMeta <$> readMetaDataFile deckerMetaFile
+  context <- initContext flags meta
+  let commands = ["clean", "purge", "example", "serve", "crunch", "crrrunch", "transcribe", "transcrrribe", "pdf", "version", "check", "format"]
+  case targets of
+    [command] | command `elem` commands -> runCommand context command rules
+    _ -> do
+      warnVersion
+      runTargets context targets rules
 
 runTargets :: ActionContext -> [FilePath] -> Rules () -> IO ()
 runTargets context targets rules = do
@@ -132,7 +127,7 @@ _runShakeSlyly context rules = do
   let meta = context ^. globalMeta
   targets <- targetsFile
   scanTargetsToFile meta targets
-  
+
   let flags = context ^. extra
   extractMetaIntoFile flags
   options <- deckerShakeOptions context
