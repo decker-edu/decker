c6e92ce64730967e26657c357d43e05b663904b2
Sebastian Lukas Hauer <sebastian.hauer@udo.edu>
2025-10-06 15:31:54 +0200

    do not add chatty button if no config available

--- resource/decker/support/plugins/chatty/chatty.js
+++ resource/decker/support/plugins/chatty/chatty.js
@@ -35,6 +35,12 @@ const Plugin = {
   id: "chatty",
   init: (deck) => {
     Reveal = deck;
+    const serverConfig = window.Decker?.meta?.chatty?.server;
+    const promptConfig = window.Decker?.meta?.chatty?.prompt;
+    if (!serverConfig || !promptConfig) {
+      console.error("Chatty config missing: ", serverConfig, promptConfig);
+      return;
+    }
     createGUI();
   },
 };
