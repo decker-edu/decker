3f0af0a7db1c554832652d8cacb5279888c2cdf2
Mario Botsch <mario.botsch@tu-dortmund.de>
2025-11-17 19:47:35 +0100

    try object tag for rendered code block

--- src/Text/Decker/Filter/Media.hs
+++ src/Text/Decker/Filter/Media.hs
@@ -566,6 +566,33 @@ renderCodeBlock uri title caption = do
     $ containOne
     $ Image imgAttr [Str fileName] (turl, "")
 
+-- embed SVG as object tag, since then a CSS file can be injected
+-- currently doesn't work on Safari...
+renderCodeBlockTEST :: (Container c) => URI -> Text -> [Inline] -> Attrib c
+renderCodeBlockTEST uri title caption = do
+  turi <- lift $ transformUri uri "svg"
+  let turl = renderUriDecode turi
+  (innerSizes, outerSizes) <- calcImageSizes
+  objectAttr <- do
+    injectClasses ["processed"]
+    injectAttribute ("data", turl)
+    injectAttribute ("type", "image/svg+xml")
+    injectStyles innerSizes
+    injectAria title caption
+    takeAttributes ["style"]
+    takeData
+    extractAttr
+  figureAttr <- do
+    injectClasses ["image rendered"]
+    cutClasses fragmentRelated >>= injectClasses
+    injectStyles outerSizes
+    takeUsual
+    extractAttr
+  return
+    $ wrapFigure figureAttr caption
+    $ mkObject objectAttr
+
+
 -- |  Transforms an image tag to script tag using the image url as src. Only
 -- supports ES6 modules.
 javascriptBlock :: (Container c) => URI -> Text -> [Inline] -> Attrib c
